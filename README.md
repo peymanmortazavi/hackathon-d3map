339e6012e5bf43dcd0ebe8087892e9dcf8baeb90
Tom Yeh <doubleshow@gmail.com>
2014-11-10 10:35:35 -0700

    starter code

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
-hackathon-d3map
-===============
+# Team Members
+
+* [name-of-a-team-member](URL to this member's github account)
+* [name-of-a-team-member](URL to this member's github account)
+* [name-of-a-team-member](URL to this member's github account)
+* [name-of-a-team-member](URL to this member's github account)
+* [name-of-a-team-member](URL to this member's github account)
+
+# Objective 1. Warmup
+
+## Star Rating --> Color
+
+![screenshot](screenshot.png)
+
+## Review Count --> Size
+
+![screenshot](screenshot.png)
+
+# Objective 2. Customization
+
+## Feature 1
+
+Implemented by: {{ name-of-a-team-member }}
+
+{{ brief description }}
+
+![screenshot](screenshot.png)
+
+![screenshot](screenshot.png)
+
+![screenshot](screenshot.png)
+
+## Feature 2
+
+Implemented by: {{ name-of-a-team-member }}
+
+{{ brief description }}
+
+![screenshot](screenshot.png)
+
+![screenshot](screenshot.png)
+
+![screenshot](screenshot.png)
+
+## Feature 3
+
+Implemented by: {{ name-of-a-team-member }}
+
+{{ brief description }}
+
+![screenshot](screenshot.png)
+
+![screenshot](screenshot.png)
+
+![screenshot](screenshot.png)
+
+## Feature 4
+
+Implemented by: {{ name-of-a-team-member }}
+
+{{ brief description }}
+
+![screenshot](screenshot.png)
+
+![screenshot](screenshot.png)
+
+![screenshot](screenshot.png)
+
+
+## Feature 5
+
+Implemented by: {{ name-of-a-team-member }}
+
+{{ brief description }}
+
+![screenshot](screenshot.png)
+
+![screenshot](screenshot.png)
+
+![screenshot](screenshot.png)
+
+
---
+++ app.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var async = require('async');
+var app = express();
+
+var mongo = require('mongoskin');
+var db = mongo.db("mongodb://localhost:27017/yelp", {native_parser:true});
+
+app.engine('.html', require('ejs').__express);
+app.set('views', __dirname);
+app.set('view engine', 'html');
+
+app.get('/query/:param1/:param2', function(req, res) {
+    var param1 = req.params.param1
+    var param2 = req.params.param2    
+    var query = {"state" : param1};
+    var projection = {};
+    db.collection('business')
+        .find(query,projection)
+        .sort( {review_count: -1 } )
+        .limit(10)
+        .toArray(function (err, businesses) {        
+           
+
+            async.map(businesses, 
+                function(business, callback){ 
+                    var query = {"business_id" : business.business_id, 
+                                 "votes.useful" : { $gt : parseInt(param2)}};
+                    db.collection('review')
+                      .find(query)
+                      .limit(10)
+                      .toArray(function (err, reviews){
+
+                        business.reviews = reviews;
+                        callback(err, business); 
+
+                      });
+                },
+                function(err, results){
+
+                   res.render("business_map", {data: results, param1: param1, param2: param2});        
+
+            });
+
+    });
+});
+
+app.listen(3000);
+console.log('listening on port 3000');
