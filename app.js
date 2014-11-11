e29c7a0af0dde272a38944819b81f470962e45e0
Peyman Mortazavi <pmortazavi@cailmobility.com>
2014-11-10 19:44:10 -0700

    Added a new query

--- app.js
+++ app.js
@@ -45,5 +45,40 @@ app.get('/query/:param1/:param2', function(req, res) {
     });
 });
 
+app.get('/query/:param1/:param2/:param3', function(req, res) {
+    var param1 = req.params.param1
+    var param2 = req.params.param2    
+    var query = {"state" : param1};
+    var projection = {};
+    db.collection('businesses')
+        .find(query,projection)
+        .sort( {review_count: -1 } )
+        .limit(10)
+        .toArray(function (err, business) {        
+           
+
+            async.map(business, 
+                function(business, callback){ 
+                    var query = {"business_id" : business.business_id, 
+                                 "stars" : { $gt : parseInt(param2)}};
+                    db.collection('review')
+                      .find(query)
+                      .limit(parseInt(param3))
+                      .toArray(function (err, reviews){
+
+                        business.reviews = reviews;
+                        callback(err, business); 
+
+                      });
+                },
+                function(err, results){
+
+                   res.render("business_map", {data: results, param1: param1, param2: param2});        
+
+            });
+
+    });
+});
+
 app.listen(3000);
 console.log('listening on port 3000');
